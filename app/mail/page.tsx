d47da5a6af69b49d03ffe71ea39b3ed46bad1930
Akinsoyinu Olumide <olumideakinsoyinu@gmail.com>
2025-02-08 18:07:22 +0100

    Build: Added About Page Content and Changed Navigation List Items

--- app/mail/page.tsx
+++ app/mail/page.tsx
@@ -13,7 +13,7 @@ export default async function MailPage() {
 
   return (
     <>
-      <div className="hidden lg:block goat">
+      <div className="hidden lg:block">
         <Mail
           accounts={accounts}
           mails={mails}
