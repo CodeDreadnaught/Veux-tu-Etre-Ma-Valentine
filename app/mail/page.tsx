49f2f3cfa2f572a40bc2095edd8549e07f9ea2f9
Akinsoyinu Olumide <olumideakinsoyinu@gmail.com>
2025-02-08 17:10:31 +0100

    Created Veux-tu Etre Ma Valentine Version 1.0

---
+++ app/mail/page.tsx
@@ -0,0 +1,36 @@
+import { cookies } from "next/headers";
+import { MobileMail } from "@/app/mail/mobile-components/mail";
+import { Mail } from "@/app/mail/desktop-components/mail";
+import { accounts, mails } from "@/app/mail/data";
+
+export default async function MailPage() {
+  const cookiesHeader = (await cookies()) as unknown as any;
+  const layout = cookiesHeader.get("react-resizable-panels:layout:mail");
+  const collapsed = cookiesHeader.get("react-resizable-panels:collapsed");
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+
+  return (
+    <>
+      <div className="hidden lg:block">
+        <Mail
+          accounts={accounts}
+          mails={mails}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
+      </div>
+      <div className="lg:hidden">
+        <MobileMail
+          accounts={accounts}
+          mails={mails}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
+      </div>
+    </>
+  );
+}
