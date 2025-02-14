c5fc53b57c0f0e8e1cd13f2eb20a56f7cb0ae106
Akinsoyinu Olumide <olumideakinsoyinu@gmail.com>
2025-02-14 18:37:49 +0100

    Feat: Added PWA Functionality

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPWA(nextConfig);
